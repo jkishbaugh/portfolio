a0b58cc6808c851e25ea37241be9bb7a82456bfb
Justin <kishju@gmail.com>
2020-09-27 00:03:53 -0600

    Initial commit

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import styles from './Resume.css'
+
+const Resume = () => {
+  return (
+
+    <div className={styles.Resume}>
+      <img src={"../../assets/resume.pdf"} alt={"resume"}/>
+    </div>
+
+  );
+
+}
+
+export default Resume;
