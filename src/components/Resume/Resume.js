52bac63f1be513ba60514b955b1a1d648707ea5d
Justin <kishju@gmail.com>
2020-10-01 07:30:01 -0600

    resume page initial

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import styles from './Resume.css'
+import styles from './Resume.module.css'
+import resume from '../../assets/resume.jpg'
 
 const Resume = () => {
   return (
 
     <div className={styles.Resume}>
-      <img src={"../../assets/resume.pdf"} alt={"resume"}/>
+      <img src={resume} alt={"resume"}/>
     </div>
 
   );
